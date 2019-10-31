43c5c764e48974756d97c1be3bd427208c8f2e14
Dat <50757817+dvtdat130999@users.noreply.github.com>
2019-10-31 23:26:00 +0700

    add giao diện quản trị

--- Admin/routes/index.js
+++ Admin/routes/index.js
@@ -5,5 +5,39 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* GET customer account page. */
+router.get('/account', function(req, res, next) {
+  res.render('customer_account', { title: 'Express' });
+});
+/* GET shop list page. */
+router.get('/shop', function(req, res, next) {
+  res.render('shop', { title: 'Express' });
+});
 
+/* GET product list of shop page. */
+router.get('/products', function(req, res, next) {
+  res.render('products', { title: 'Express' });
+});
+
+/* GET sales page. */
+router.get('/sales', function(req, res, next) {
+  res.render('sales', { title: 'Express' });
+});
+
+router.get('/sales_year', function(req, res, next) {
+  res.render('sales', { title: 'Express' });
+});
+
+router.get('/sales_month', function(req, res, next) {
+  res.render('sales', { title: 'Express' });
+});
+
+router.get('/sales_week', function(req, res, next) {
+  res.render('sales', { title: 'Express' });
+});
+
+/* GET top-selling page. */
+router.get('/top', function(req, res, next) {
+  res.render('top_selling', { title: 'Express' });
+});
 module.exports = router;
